f0f29234b7528e629dd45fb6079ed9884ce2eba4
c21xdx <c21xdx@gmail.com>
2025-03-19 14:34:27 +0800

    Update vless.ts

--- netlify/functions/vless.ts
+++ netlify/functions/vless.ts
@@ -234,8 +234,15 @@ export const handler = async (request: Request, context: Context) => {
     };
 
     try {
-        // 修正 URL 处理
-        const url = new URL(request.url, 'http://' + request.headers.get('host') || 'localhost');
+        // 修改 URL 处理方式
+        let urlString = request.url;
+        if (!urlString.startsWith('http')) {
+            // 从请求中获取 host
+            const host = request.headers?.host || context.ip || 'localhost';
+            urlString = `http://${host}${urlString}`;
+        }
+        const url = new URL(urlString);
+        
         const pathMatch = url.pathname.match(new RegExp(`${SETTINGS.XHTTP_PATH}/([^/]+)(?:/([0-9]+))?$`));
         
         if (!pathMatch) {
